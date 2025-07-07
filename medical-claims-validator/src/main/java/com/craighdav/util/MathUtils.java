256da2b58ee59b19945a8d47a12decab226020ce
Craig H. Davis <craighdav@msn.com>
2025-07-07 17:06:03 -0400

    Made refinements to MathUtils and added unit testing.

--- medical-claims-validator/src/main/java/com/craighdav/util/MathUtils.java
+++ medical-claims-validator/src/main/java/com/craighdav/util/MathUtils.java
@@ -1,8 +1,28 @@
 package com.craighdav.util;
 
+/**
+ * This class is purposed to provide static utility methods for numeric operations.
+ * 
+ * Class MathUtils is designed to be stateless, providing only static methods to assist
+ * with numeric computations.
+ */
 public class MathUtils {
 
+		/**
+		 * This method extracts the leftmost digit of a long value.
+		 * 
+		 * Method  getLeftmostDigit returns the leftmost digit of any long value between
+		 * Long.MIN_VALUE + 1 and Long.MAX_VALUE. (Since the method converts negative input
+		 * values to positive values using Math.abs() and there is no positive value within
+		 * the range of long values corresponding to Long.MIN_VALUE due to the storage mechanism
+		 * of long values, the minimum negative value that can be evaluated is Long.MIN_VALUE + 1.)
+		 * 
+		 * @param longValue The long value to be evaluated
+		 * @return The leftmost digit of longValue
+		 */
 		public static int getLeftmostDigit(long longValue) {
+			
+			longValue = Math.abs(longValue);
 			
 			if (longValue > 1_000_000_000_000L) {
 				longValue /= 1_000_000_000_000L;
@@ -16,7 +36,7 @@ public class MathUtils {
 				longValue /= 1_000L;
 			}
 			
-			while (longValue > 10) {
+			while (longValue >= 10) {
 				longValue /= 10;
 			}
 			
